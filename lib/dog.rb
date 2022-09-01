cbc065ec86a93bf2604b5bcbdcd9e75db47d14ca
John J Setlock II <johnsetlock@gmail.com>
2022-09-01 16:06:04 -0500

    commit tree 0f6f3dcfaf06e07ecabcdf9c6f2f7456c87f8a1b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,24 @@
-class Dog
+require 'pry'
+require "active_record"
+
+# Setup a database connection
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/test.sqlite"
+)
+
+# # Create a table
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS dogs (
+#     id INTEGER PRIMARY KEY,
+#     name TEXT
+#   )
+# SQL
+# ActiveRecord::Base.connection.execute(sql)
+
+class Dog <ActiveRecord::Base
+
 end
+
+
+binding.pry
